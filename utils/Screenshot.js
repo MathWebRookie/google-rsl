d9929ed9ac8707fc2e50f89198d3b444adc95128
renshulong <renshulong@msunhealth.com>
2024-12-20 12:00:00 +0800

    feat：可视区域下载

--- utils/Screenshot.js
+++ utils/Screenshot.js
@@ -2,8 +2,9 @@
 export async function Screenshotinit() {
   const Screenshot = document.getElementById("Screenshot");
   const screenshotMenu = document.getElementById("screenshotMenu");
-  const imageFormatSelect = document.getElementById('imageFormat');
-  const exportFormatSelect = document.getElementById('imgexportFormat');
+  const imageFormatSelect = document.getElementById("imageFormat");
+  const exportFormatSelect = document.getElementById("imgexportFormat");
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   // 点击其他地方关闭菜单
   document.addEventListener("click", () => {
     screenshotMenu.style.display = "none";
@@ -20,19 +21,27 @@ export async function Screenshotinit() {
     screenshotMenu.style.left = `${btnRect.left}px`;
   });
   // 处理各种截图选项
-  document.getElementById('fullPage').addEventListener('click', (e) => {
+  document.getElementById("fullPage").addEventListener("click", (e) => {
     // e.stopPropagation();
     // 实现整页截图逻辑
-    console.log('整页截图',imageFormatSelect.value,exportFormatSelect.value);
+    console.log("整页截图", imageFormatSelect.value, exportFormatSelect.value);
   });
 
-  document.getElementById('visibleArea').addEventListener('click', () => {
+  document.getElementById("visibleArea").addEventListener("click", () => {
     // 实现可视区域截图逻辑
-    console.log('可视区域截图');
+    chrome.tabs.captureVisibleTab(
+      tab.windowId,
+      { format: "png", quality: 100 },
+      (image) => {
+        // 会返回 base64 图片
+        console.log("image:", image);
+      }
+    );
+    console.log("可视区域截图");
   });
 
-  document.getElementById('selectArea').addEventListener('click', () => {
+  document.getElementById("selectArea").addEventListener("click", () => {
     // 实现选择区域截图逻辑
-    console.log('选择区域截图');
+    console.log("选择区域截图");
   });
 }
