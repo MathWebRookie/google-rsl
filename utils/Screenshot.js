e542f2f2fc111e7805edfe281989466be78dc3f9
renshulong <renshulong@msunhealth.com>
2024-12-20 11:26:49 +0800

    feat: 截图调整

--- utils/Screenshot.js
+++ utils/Screenshot.js
@@ -2,6 +2,8 @@
 export async function Screenshotinit() {
   const Screenshot = document.getElementById("Screenshot");
   const screenshotMenu = document.getElementById("screenshotMenu");
+  const imageFormatSelect = document.getElementById('imageFormat');
+  const exportFormatSelect = document.getElementById('imgexportFormat');
   // 点击其他地方关闭菜单
   document.addEventListener("click", () => {
     screenshotMenu.style.display = "none";
@@ -16,5 +18,21 @@ export async function Screenshotinit() {
     const btnRect = Screenshot.getBoundingClientRect();
     screenshotMenu.style.top = `${btnRect.bottom + 5}px`;
     screenshotMenu.style.left = `${btnRect.left}px`;
+  });
+  // 处理各种截图选项
+  document.getElementById('fullPage').addEventListener('click', (e) => {
+    // e.stopPropagation();
+    // 实现整页截图逻辑
+    console.log('整页截图',imageFormatSelect.value,exportFormatSelect.value);
+  });
+
+  document.getElementById('visibleArea').addEventListener('click', () => {
+    // 实现可视区域截图逻辑
+    console.log('可视区域截图');
+  });
+
+  document.getElementById('selectArea').addEventListener('click', () => {
+    // 实现选择区域截图逻辑
+    console.log('选择区域截图');
   });
 }
