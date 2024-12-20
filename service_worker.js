7f790755e13bfe50584f6bc8d756f938cecf50c5
renshulong <renshulong@msunhealth.com>
2024-12-20 15:05:02 +0800

    feat： dom截图实现

--- service_worker.js
+++ service_worker.js
@@ -1,15 +1,16 @@
-// // 生成随机颜色的函数
-// function getRandomColor() {
-//   const letters = '0123456789ABCDEF';
-//   let color = '#';
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
-
-// // 在插件安装时，存储随机生成的颜色
-// chrome.runtime.onInstalled.addListener(() => {
-//   const randomColor = getRandomColor();
-//   chrome.storage.sync.set({ color: randomColor });
-// });
+// 监听返回整个图片
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.type === "screenshot") {
+      chrome.tabs.captureVisibleTab(
+        //   tab.windowId,
+        { format: "png", quality: 100 },
+        (image) => {
+          // 会返回 base64 图片
+          console.log("image1111111:", image);
+          res({ image });
+        }
+      );
+    }
+    return true;
+  });
+  
